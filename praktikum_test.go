f43b1390f4cb43aab9bc340ae2eb7ac6346a53ec
indrariksa <indrariksa@gmail.com>
2024-03-22 09:15:50 +0700

    first

---
+++ praktikum_test.go
@@ -0,0 +1,27 @@
+package _2345
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInsertPresensi(t *testing.T) {
+	long := 98.345345
+	lat := 123.561651
+	lokasi := "Rumah"
+	phonenumber := "68122221814"
+	checkin := "masuk"
+	biodata := Karyawan{
+		Nama:         "Drake",
+		Phone_number: "628456456211",
+		Jabatan:      "Rakyat Biasa",
+	}
+	hasil := InsertPresensi(long, lat, lokasi, phonenumber, checkin, biodata)
+	fmt.Println(hasil)
+}
+
+func TestGetKaryawanFromPhoneNumber(t *testing.T) {
+	phonenumber := "08123456789"
+	biodata := GetKaryawanFromPhoneNumber(phonenumber)
+	fmt.Println(biodata)
+}
